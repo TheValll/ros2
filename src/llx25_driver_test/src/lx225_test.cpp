6cd003c7788996299c5f3022662b285c3857b958
TheValll <valentin78.massonniere@gmail.com>
2026-03-05 16:08:14 +0100

    lx225 driver

--- src/llx25_driver_test/src/lx225_test.cpp
+++
@@ -1,6 +0,0 @@
-#include "rclpp/rclcpp.hpp"
-#include "lx225_driver_test/LX225Driver.hpp"
-
-int main(){
-  return 0;
-}
--- src/lx225_driver_test/src/lx225_test.cpp
+++ src/lx225_driver_test/src/lx225_test.cpp
@@ -1,3 +1,6 @@
-#include "rclpp/rclcpp.hpp"
+#include "rclcpp/rclcpp.hpp"
 #include "lx225_driver_test/LX225Driver.hpp"
 
+int main(){
+  return 0;
+}

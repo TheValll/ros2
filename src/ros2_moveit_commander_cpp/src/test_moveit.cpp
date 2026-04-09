b1cdb4bf295a7035f350743d2372c19a86b761b4
TheValll <valentin78.massonniere@gmail.com>
2026-04-09 17:11:46 +0200

    ros2 commander

--- src/ros2_moveit_commander_cpp/src/test_moveit.cpp
+++ src/ros2_moveit_commander_cpp/src/test_moveit.cpp
@@ -15,8 +15,59 @@ int main(int argc, char **argv)
   arm.setMaxAccelerationScalingFactor(1.0);
 
   // Named goal
+  // arm.setStartStateToCurrentState();
+  // arm.setNamedTarget("pose_1");
+  //
+  // moveit::planning_interface::MoveGroupInterface::Plan plan1;
+  // bool success = (arm.plan(plan1) == moveit::core::MoveItErrorCode::SUCCESS);
+  //
+  // if(success){
+  //   arm.execute(plan1);
+  // }
+  //
+  // arm.setStartStateToCurrentState();
+  // arm.setNamedTarget("home");
+  //
+  // moveit::planning_interface::MoveGroupInterface::Plan plan2;
+  // bool success2 = (arm.plan(plan2) == moveit::core::MoveItErrorCode::SUCCESS);
+  //
+  // if(success2){
+  //   arm.execute(plan2);
+  // }
+  
+
+  // Joint goal
+  
+  // std::vector<double> joints = {1.5, 0.5, 0.0, 1.5, 0.0, -0.7};
+  //
+  // arm.setStartStateToCurrentState();
+  // arm.setJointValueTarget(joints);
+  //
+  // moveit::planning_interface::MoveGroupInterface::Plan plan1;
+  // bool success = (arm.plan(plan1) == moveit::core::MoveItErrorCode::SUCCESS);
+  //
+  // if(success){
+  //   arm.execute(plan1);
+  // }
+  
+  // Pose goal
+  
+  tf2::Quaternion q;
+  q.setRPY(3.14, 0.0, 0.0);
+  q = q.normalize();
+
+  geometry_msgs::msg::PoseStamped target_pose;
+  target_pose.header.frame_id = "base_link";
+  target_pose.pose.position.x = 0.0;
+  target_pose.pose.position.y = -0.7;
+  target_pose.pose.position.z = 0.4;
+  target_pose.pose.orientation.x = q.getX();
+  target_pose.pose.orientation.y = q.getY();
+  target_pose.pose.orientation.z = q.getZ();
+  target_pose.pose.orientation.w = q.getW();
+
   arm.setStartStateToCurrentState();
-  arm.setNamedTarget("pose_1");
+  arm.setPoseTarget(target_pose);
 
   moveit::planning_interface::MoveGroupInterface::Plan plan1;
   bool success = (arm.plan(plan1) == moveit::core::MoveItErrorCode::SUCCESS);
@@ -25,14 +76,26 @@ int main(int argc, char **argv)
     arm.execute(plan1);
   }
 
-  arm.setStartStateToCurrentState();
-  arm.setNamedTarget("home");
+  // Cartesian  Path
 
-  moveit::planning_interface::MoveGroupInterface::Plan plan2;
-  bool success2 = (arm.plan(plan2) == moveit::core::MoveItErrorCode::SUCCESS);
+  std::vector<geometry_msgs::msg::Pose> waypoints;
+  geometry_msgs::msg::Pose pose1 = arm.getCurrentPose().pose;
+  pose1.position.z += -0.2;
+  waypoints.push_back(pose1);
+  geometry_msgs::msg::Pose pose2 = pose1;
+  pose2.position.y += 0.2;
+  waypoints.push_back(pose2);
+  geometry_msgs::msg::Pose pose3 = pose2;
+  pose3.position.y += -0.2;
+  pose3.position.z += 0.2;
+  waypoints.push_back(pose3);
 
-  if(success2){
-    arm.execute(plan2);
+  moveit_msgs::msg::RobotTrajectory trajectory;
+
+  double fraction = arm.computeCartesianPath(waypoints, 0.01, trajectory);
+
+  if (fraction == 1){
+    arm.execute(trajectory);
   }
 
   rclcpp::shutdown();
